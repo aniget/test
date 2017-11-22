6ab50b1ed7137f49067c2b8799007e0acad72e44
Unknown <ahristoff@abv.bg>
2017-11-22 12:31:11 +0200

    First

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello!");
+
+        }
+    }
+}
